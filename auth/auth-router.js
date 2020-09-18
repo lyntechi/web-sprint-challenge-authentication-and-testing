31992f6ef90a15f9199f4c725e7a44af894f67a1
Lynda Santiago <lyntechi@gmail.com>
2020-09-18 12:21:13 -0400

    jokes successfully retrieved after user logs in with token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const router = require("express").Router();
+const Users = require("../users/users-model");
+const jwt = require("jsonwebtoken");
 
 function isValid(user) {
   return Boolean(
@@ -12,13 +14,70 @@ router.post("/register", (req, res) => {
 
   if (isValid(credentials)) {
     const rounds = process.env.BCRYPT_ROUNDS || 8;
-    const hash = bcryptjs.hashSync(credentials.password, rounds)
+    //this is me hashing the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
     credentials.password = hash;
+
+    //now i am saving user to the database
+    Users.add(credentials)
+      .then((user) => {
+        console.log("this is user", user);
+        res.status(201).json({ data: user });
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
+    });
   }
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // if (user && bcryptjs.compareSync(password, user.password)) {
+        //   res.json({ message: "user logged in" });
+        // } else {
+        //   res.status(401).json({ message: "invalid credentials" });
+        // }
+
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = makeJwt(user);
+          res.status(200).json({ token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
+
+function makeJwt({ id, username, department }) {
+  const payload = {
+    username,
+    subject: id,
+  };
+  const config = {
+    jwtSecret: process.env.JWT_SECRET || "is it secret, is it safe?",
+  };
+  const options = {
+    expiresIn: "8 hours",
+  };
+  return jwt.sign(payload, config.jwtSecret, options);
+}
 
 module.exports = router;
